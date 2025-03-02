9860fff763861e79b509725e6424bf1f686c122f
Monika Kiskinova <monikakiskinova@gmail.com>
2025-03-02 11:35:07 +0200

    Extract main components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return (
+        <>
+            <header className="header">
+                <div className="logo">
+                    <span className="course">React Workshop - January 2025</span>
+                </div>
+            </header>
+        </>
+    )
+}
